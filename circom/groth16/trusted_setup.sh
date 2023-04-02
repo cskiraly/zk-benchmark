9b66b7882c59ee26ef95a7ed0baa9085da843f1b
Csaba Kiraly <csaba.kiraly@gmail.com>
2023-04-02 11:01:30 +0200

    update trusted setup to generate plonk and fflonk
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- circom/groth16/trusted_setup.sh
+++ circom/groth16/trusted_setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
   echo "tau_rank required"
   exit 1
 fi
 
 SCRIPT=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT")
-CIRCUIT_DIR=${SCRIPT_DIR}"/../circuits/sha256_test"
-TAU_RANK=$1
+CIRCUIT_NAME=$1
+CIRCUIT_DIR=${SCRIPT_DIR}"/../circuits/${CIRCUIT_NAME}_test"
+TAU_RANK=$2
 TAU_DIR=${SCRIPT_DIR}"/../setup/tau"
 TAU_FILE="${TAU_DIR}/powersOfTau28_hez_final_${TAU_RANK}.ptau"
 
@@ -17,7 +18,9 @@ if [ ! -f "$TAU_FILE" ]; then
 fi
 
 pushd "$CIRCUIT_DIR" || exit
-snarkjs groth16 setup sha256_test.r1cs ${TAU_FILE} sha256_test_0000.zkey
-echo 1 | snarkjs zkey contribute sha256_test_0000.zkey sha256_test_0001.zkey --name='Celer' -v
-snarkjs zkey export verificationkey sha256_test_0001.zkey verification_key.json
+snarkjs fflonk setup ${CIRCUIT_NAME}_test.r1cs ${TAU_FILE} ${CIRCUIT_NAME}_test_fflonk.zkey
+snarkjs plonk setup ${CIRCUIT_NAME}_test.r1cs ${TAU_FILE} ${CIRCUIT_NAME}_test_plonk.zkey
+snarkjs groth16 setup ${CIRCUIT_NAME}_test.r1cs ${TAU_FILE} ${CIRCUIT_NAME}_test_0000.zkey
+echo 1 | snarkjs zkey contribute ${CIRCUIT_NAME}_test_0000.zkey ${CIRCUIT_NAME}_test_0001.zkey --name='Celer' -v
+snarkjs zkey export verificationkey ${CIRCUIT_NAME}_test_0001.zkey verification_key.json
 popd || exit
